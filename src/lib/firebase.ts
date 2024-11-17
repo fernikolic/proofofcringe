4cd489f0031bbcc295d0b2af52e18b82d8ef8430
Fernando Nikolic <fernandonikolic@Fernandos-MacBook-Pro.local>
2024-11-17 14:48:03 +0100

    Deleted all files

--- src/lib/firebase.ts
+++
@@ -1,9 +0,0 @@
-import { initializeApp } from 'firebase/app';
-import { getDatabase } from 'firebase/database';
-
-const firebaseConfig = {
-  databaseURL: 'https://proof-of-cringe-72a1a-default-rtdb.firebaseio.com'
-};
-
-export const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app);
